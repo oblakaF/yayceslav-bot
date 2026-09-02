a3b60c58bd1bf4d7c0e072ac0deb52242be82cff
oblakaF <vadimakrysko@gmail.com>
2026-09-02 19:26:03 +0800

    Register movie recommendation vertical

--- identity_recommendation_bootstrap_patch.py
+++ identity_recommendation_bootstrap_patch.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import functools
 
 import book_recommendation_runtime
+import movie_recommendation_runtime
 import natural_router_runtime
 
 
@@ -25,6 +26,7 @@ def install() -> bool:
     def prepare_then_identity_recommendations(application):
         original(application)
         book_recommendation_runtime.prepare_application_runtime(application)
+        movie_recommendation_runtime.prepare_application_runtime(application)
 
     prepare_then_identity_recommendations._yayceslav_identity_recommendation_hook = True
     natural_router_runtime.prepare_application_runtime = prepare_then_identity_recommendations
